4c5b54febdeca8511cd7a879317912d3319e8579
Aurino Geraldo <aurinoaj@gmail.com>
2021-11-08 22:26:44 -0300

    Add header's elements

--- 04-dashgo/src/components/Header.tsx
+++ 04-dashgo/src/components/Header.tsx
@@ -1,5 +1,18 @@
-import { Flex, Text, Input as ChakraInput, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri";
+import {
+  Flex,
+  Text,
+  Input as ChakraInput,
+  Icon,
+  HStack,
+  Box,
+  Avatar,
+} from "@chakra-ui/react";
+
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 
 export function Header() {
   return (
@@ -44,6 +57,32 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20" cursor="text" />
+      </Flex>
+
+      <Flex align="center" marginLeft="auto">
+        <HStack
+          spacing="8"
+          marginX="8"
+          paddingRight="8"
+          paddingY="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box>
+            <Text>Aurino Junior</Text>
+            <Text color="gray.300" fontSize="small">
+              aurino@gmail.com
+            </Text>
+          </Box>
+        </Flex>
+
+        <Avatar marginLeft="4" size="md" name="Aurino Junior" />
       </Flex>
     </Flex>
   );
